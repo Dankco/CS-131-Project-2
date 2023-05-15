dc6f1487be390aafaadc16961ed2dd3e401ec0de
Daniel Kao <dack8@outlook.com>
2023-05-15 01:04:48 -0700

    add interpreter

--- tester.py
+++ tester.py
@@ -188,7 +188,7 @@ def generate_test_suite_v2():
             "test_inher1",
             "test_let",
         ],
-        ["test_incompat_return1", "test_let2", "test_inher1", "test_incompat_types2"],
+        ["test_incompat_return1", "test_let2", "test_inher1", "test_incompat_types2", "test_static_types"],
     )
 
 
